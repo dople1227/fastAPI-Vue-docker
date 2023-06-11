283b5b1b039dae9c50673057c83d51463b3b16ac
soronprfbs <dople1227@gmail.com>
2023-06-11 20:00:51 +0900

    add pytest

--- services/backend/src/app/main.py
+++ services/backend/src/app/main.py
@@ -1,18 +1,55 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from enum import Enum
 
 app = FastAPI()
 
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["http://localhost:8080", "http://localhost:8081"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
+class ModelName(str, Enum):
+    """독스트링 스타일
+
+    첫줄에 요약된 설명을 하고 추가설명은 한줄 띄운 후 기입
+    """
+
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/ping")
+def pong():
+    return {"message": "Ping Pong!"}
+
+
 @app.get("/")
-def home():
-    return "Hello, World!"
+def root():
+    return {"message": "Hello, World!"}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
